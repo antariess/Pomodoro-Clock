fed57b0b06e651f1e28b5436e4b2a000def73a5d
Vel Georgieva <velislava.n.georgieva@gmail.com>
2018-02-15 14:17:07 +0000

    Created files, links and basic code
    html - created all needed elements
    css - pseudocode and background colour
    js - pseudocode of functions to come

---
+++ pomodoroClock.js
@@ -0,0 +1,7 @@
+// link with jquery
+// set time out function to decrease the counter with 00:01
+// - and + buttons to add a minute to timer 
+// start button - calls the countdown function
+// reset button - sets times and break back to initia values
+// pause button - pauses countdown temporarily
+// stop button - breaks out of coutdown permanently
