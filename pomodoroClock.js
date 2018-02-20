31fe8d8eb1dd77e0c26b4d75f4fe87b2924e7120
Vel Georgieva <velislava.n.georgieva@gmail.com>
2018-02-20 15:40:45 +0000

    work seconds and pause
    setInterval working for seconds on 'work' timer, pause button working

--- pomodoroClock.js
+++ pomodoroClock.js
@@ -5,3 +5,32 @@
 // reset button - sets times and break back to initia values
 // pause button - pauses countdown temporarily
 // stop button - breaks out of coutdown permanently
+
+//WORK
+//seconds
+const $secsWork = $("#secsWork")
+const $secsWorkNum = parseInt($secsWork.html(), 10)
+let secsWorkCount = $secsWorkNum
+const count = function(){    
+    if (secsWorkCount <= 0) {
+        clearTimeout(interval)
+    }
+    console.log(secsWorkCount)    
+    $secsWork.html(secsWorkCount)
+    secsWorkCount--
+}
+
+const interval = setInterval(count, 1000)
+
+// const counting = function(){
+//     while (a.text()<=0){
+//         console.log("I got here")
+//         setTimeout(count, 1000)
+//     }
+// }
+
+// counting()
+
+$("#pause").click(function(){
+    clearTimeout(interval)
+})
