780b0cec8a0d4332c4ec4c50b008d1b2399b5e3e
Vel Georgieva <velislava.n.georgieva@gmail.com>
2018-02-21 21:50:33 +0000

    increase/decrease
    Created increase and decrease work timer functionality. Updated Reset, Play and Pause buttons to update min and sec trackers. Created a state variable to check if the timer is running and prevent incrementations buttons from running if it is.

--- pomodoroClock.js
+++ pomodoroClock.js
@@ -1,25 +1,20 @@
-// link with jquery
-// set time out function to decrease the counter with 00:01
 // - and + buttons to add a minute to timer 
-// start button - calls the countdown function
-// reset button - sets times and break back to initia values
-// pause button - pauses countdown temporarily
-// stop button - breaks out of coutdown permanently
 
 //WORK
-//seconds
+let intervalState = false
 const $secsWork = $("#secsWork")
-const $secsWorkNum = parseInt($secsWork.html(), 10)
-
-//minutes
 const $minsWork = $("#minsWork")
-const $minsWorkNum = parseInt($minsWork.html(), 10)
-let seconds = $minsWorkNum * 60
+const parse = function(span){
+    return parseInt(span.html(), 10)
+}
+let seconds; 
+let minsTracker; 
 let secTracker = 59 
-let minsTracker = $minsWorkNum 
 const count = function(){ 
+    intervalState = true
     if (seconds <= 0) {
         clearTimeout(intervalSec)
+        intervalState = false
         return
     }
     if (seconds % 60 == 0){
@@ -35,19 +30,46 @@ const count = function(){
 }
 var intervalSec;
 
+//increase decreace time
+const $incrWork = $("#incrWork")
+const $decrWork = $("#decrWork")
+const $incrBreak = $("#incrBreak")
+const $decrBreak = $("#decrBreak")
+const increase = function(){
+    if (!intervalState){
+        if (parse($minsWork) < 59){
+            $minsWork.html(parse($minsWork) + 1)
+        }
+    }    
+}
+const decrease = function(){
+    if (!intervalState){
+        if(parse($minsWork) > 0){
+            $minsWork.html(parse($minsWork) - 1)
+        }
+    }    
+}
+$incrWork.click(increase)
+$decrWork.click(decrease)
+
 //buttons
 const $play = $("#play")
 $play.click(function(){
+    seconds = parse($minsWork) * 60 + parse($secsWork); 
+    minsTracker = parse($minsWork)
     intervalSec = setInterval(count, 100)
 })
 
 const $pause = $("#pause")
 $pause.click(function(){
+    intervalState = false
     clearTimeout(intervalSec)
 })
 
 const $reset = $("#reset")
 $reset.click(function(){
+    intervalState = false
+    clearTimeout(intervalSec)
     $secsWork.html(00)
     $minsWork.html(25)
 })
